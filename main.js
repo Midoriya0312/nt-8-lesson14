da81daaca5cf36a46010b5bb30aed477ddea91e1
Midoriya0312 <oybektojiboyev0312@gmail.com>
2023-07-16 15:36:26 +0500

    lesson14

---
+++ main.js
@@ -0,0 +1,142 @@
+// let time = new Date();
+
+// let vaqt = time.getHours()
+// console.log(vaqt);
+
+// let is_shop_open = false;
+
+// if(vaqt >= 10) {
+//     is_shop_open = true
+// }
+
+// let stock = {
+//     Fruits: ['strawberry', 'grapes', 'banana', 'apple'],
+//     liquid: ['wate', 'ice'],
+//     holder: ['cone', 'cup', 'stick'],
+//     toppings: ['shocolate', 'peanuts'],
+// }
+
+// let order = (time, work) => {
+//     return new Promise((res, rej) => {
+//         if(is_shop_open) {
+//             setTimeout(() => {
+//                 res(work())
+//             },time)
+//         }else {
+//             rej(console.log('dokon yopildi'))
+//         }
+//     })
+// }
+
+// order(2000, () => {
+//     console.log('started');
+// })
+
+//     .then(() => { 
+//        return order (2000, () => console.log('f choped'))
+//     })
+//     .then(() => {
+//         return order(2000, () => console.log('ice cream'))
+//     })
+
+
+
+
+
+
+// Homework process
+
+// 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Homework localStorage
+
+
+// 1
+
+// localStorage.setItem('name', 'ali');
+    
+
+// 2
+
+// console.log(localStorage.getItem('name'));
+
+// 3
+
+// localStorage.setItem('surname', 'valiyev')
+
+// 4
+
+// console.log(localStorage.getItem('surname'));
+
+// 5
+
+// localStorage.setItem('brend', 'samsung');
+
+// 6
+
+// console.log(localStorage.getItem('brend'));
+
+// 7
+
+// localStorage.clear()
+
+// 8
+
+// localStorage.setItem('nt-8', 'Tojiboyev Oybek')
+
+// 9
+
+// console.log(localStorage.getItem('nt-8'));
+
+// 10
+
+// localStorage.setItem('Uzbekistan', 'Tashkent')
+
+// 11
+
+// console.log(localStorage.getItem('Uzbekistan'));
+
+// 12
+
+// localStorage.setItem('english', '26');
+
+// 13
+
+// localStorage.getItem('english');
+
+// 14
+
+// localStorage.setItem('russia', '32');
+
+// 15
+
+// console.log(localStorage.getItem('russia'));
+
+// 16
+
+// localStorage.setItem('brend', 'xiaomi');
+
+// 17
+
+// console.log(localStorage.getItem('brend'));
+
+// 18
+
